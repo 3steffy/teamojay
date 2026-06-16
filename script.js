e4ce6be8b6f5377df6230250826c6a3ca67510a8
3steffy <3steffyrodriguez@gmail.com>
2026-06-16 15:27:50 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+const boton=document.getElementById("abrir");
+
+const inicio=document.getElementById("inicio");
+
+const contenido=document.getElementById("contenido");
+
+const musica=document.getElementById("musica");
+
+boton.onclick=()=>{
+
+inicio.style.display="none";
+
+contenido.style.display="block";
+
+musica.play();
+
+};
+
+function crearCorazon(){
+
+const corazon=document.createElement("div");
+
+corazon.classList.add("heart");
+
+corazon.innerHTML="❤";
+
+corazon.style.left=Math.random()*100+"vw";
+
+corazon.style.animationDuration=(Math.random()*3+4)+"s";
+
+document.body.appendChild(corazon);
+
+setTimeout(()=>{
+
+corazon.remove();
+
+},7000);
+
+}
+
+setInterval(crearCorazon,250);
